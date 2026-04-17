96417532fec4a9d11f27667c48ad2005e060d6ea
Oppie1 <atindall729@gmail.com>
2026-04-17 18:36:00 -0500

    Add files via upload

---
+++ 1.3CCmputrPrgramsWrkHelloWorld.Claude/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+
+/*This program prints a greeting message to the screen*/
+
+int main()
+
+{
+	/*Print "Hello Claudia! with a bell sound and move to a new line*/
+	printf("Hello Claudia!\a\n");
+
+	/*Print a message on the next line*/
+	printf("Claudia is cool");
+
+	return 0;
+}
